c72d91b0bc9f329d4cab8f8108242f0a7cbcc6e8
ggkannan <ggkannan@gmail.com>
2018-02-03 17:45:46 +0530

    sass changes

--- dist/assets/node-asset-manifest.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-define('dummy/config/node-asset-manifest', function() {
-  return {
-    default: {"bundles":{"child-app":{"assets":[{"uri":"\\engines-dist\\child-app\\assets\\engine-vendor.js","type":"js"},{"uri":"\\engines-dist\\child-app\\assets\\engine.js","type":"js"}]}}}
-  };
-});
